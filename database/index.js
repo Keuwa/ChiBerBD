3e96a8603539b33115dd2b502beda18bbf7bfc0c
Keuwa <vincent.durpoix@hotmail.fr>
2019-04-19 04:55:48 +0200

    aze

--- database/index.js
+++ database/index.js
@@ -1,130 +1,94 @@
 var levelup = require('levelup')
 var leveldown = require('leveldown')
+var fs = require("fs")
+var path = require('path')
+
+var filePath = path.join(__dirname, 'database.json');
+
 
 var timeTotal = 10
 
 var db = levelup(leveldown('./mydb'))
 
 function setWinner(player){
-    console.log("helloaze");
-    console.log(player)
+    fs.readFile(filePath, {encoding: 'utf-8'}, function(err,data){
+        if (!err) {
+            data = JSON.parse(data)
+            data[player].end = Date.now()
+            fs.writeFile(filePath,JSON.stringify(data),(err)=>{
+                if (err) console.log(err)
+                
+            })
+        } else {
+            console.log(err)
+        }
+    }); 
     
-    db.put(player+"End", Date.now(),function(err,value){
-        if (err) res.send(err)
-    })
 }
 
 module.exports = {
     initDb: (res)=> {
         var dateNow = Date.now()
-        
-        
-        setTimeout(function(){ 
-            db.del("timer", function (err) {
-                if (err) res.send({"err11":err.toString()})
-            })
-            db.del("chitaiRep", function (err) {
-                if (err) res.send({"err12":err.toString()})
-            })
-            db.del("bernieeeeRep", function (err) {
-                if (err) res.send({"err13":err.toString()})
-            })
-            db.del("chitaiEnd", function (err) {
-                if (err) res.send({"err14":err.toString()})
-            })
-            db.del("bernieeeeEnd", function (err) {
-                if (err) res.send({"err15":err.toString()})
-            })
-            db.del("bernieeeeQuestion", function (err) {
-                if (err) res.send({"err16":err.toString()})
-            })
-            db.del("chitaiQuestion", function (err) {
-                if (err) res.send({"err17":err.toString()})
-            })
-    
-            db.put("timer",dateNow, function (err) {
-                if (err) res.send({"err18":err.toString()})
-            })
-    
-            db.put('chitaiRep', 0, function (err) {
-                if (err) res.send({"err2":err.toString()})
-            })
-    
-            db.put('bernieeeeRep',0 , function (err) {
-                if (err) res.send({"err2":err.toString()})
-            })
-    
-            let chitaiQuestion = {
-                "questions":[
-                    "À abaisser en cas d'urgence. Mais n'arretera pas la légion",
-                    "Si A ne dis pas bonjour a B. Quel membre de la famille de A doit être niquer",
-                    "Quand tu cherches, il n'y a qu'une chose \"HERE\"",
-                    "J'ai trouvé un document entre deux salles de test. Il était écrit \"Genetic Lifeform and Disk Operating System\". Je ne sais pas ce que ça veut dire mais j'attends toujours mon gateau",
-                    "Agent de la division par deux, bienvenu à la maison blanche nous avons besoin de sécuriser la safe room dans la zone au sud de notre position.<br> J'espère pour vous que vous n'avez pas perdu votre carte",
-                    "[FR]Survivant avec un invité mystherbe ! - 1:27:17"
-                ],
-                "answers":[
-                    "palette",
-                    "mère",
-                    "mozambique",
-                    "glados",
-                    "safe house",
-                    "smirk"
-                ]
+        var data = {
+            "timer":dateNow,
+            "bernieeee":{
+                "end":99999999999999999999999,
+                "rep":0,
+                "question":{
+                    "questions":[
+                        "Et donc sur",
+                        "La go la c'est peut etre une fille bien. Mais que preferons nous ?",
+                        "Vague, pierre, fantome, loing",
+                        "A l'ultimate, on cherche à l'éviter à tout prix. Mais dans un RPG c'est souvent une bonne nouvelle",
+                        "Cette chanson est vraiment la plus dure de toute... Je n'arrive même pas les 3 premières couleurs...",
+                        "<p id=\"song\" > Oh shit soraka i'm gonna die !! p> 235s"
+                    ],
+                    "answers":[
+                        "hoth",
+                        "tchoin",
+                        "tsunami",
+                        "drop",
+                        "rouge vert jaune",
+                        "mocking shout"
+                    ]
+                }
+            },
+            "chitai":{
+                "end":99999999999999999999999,
+                "rep":0,
+                "question": {
+                    "questions":[
+                        "À abaisser en cas d'urgence. Mais n'arretera pas la légion",
+                        "Si A ne dis pas bonjour a B. Quel membre de la famille de A doit être niquer",
+                        "Quand tu cherches, il n'y a qu'une chose \"HERE\"",
+                        "J'ai trouvé un document entre deux salles de test. Il était écrit \"Genetic Lifeform and Disk Operating System\". Je ne sais pas ce que ça veut dire mais j'attends toujours mon gateau",
+                        "Agent de la division par deux, bienvenu à la maison blanche nous avons besoin de sécuriser la safe room dans la zone au sud de notre position.<br> J'espère pour vous que vous n'avez pas perdu votre carte",
+                        "[FR]Survivant avec un invité mystherbe ! - 1:27:17"
+                    ],
+                    "answers":[
+                        "palette",
+                        "mère",
+                        "mozambique",
+                        "glados",
+                        "safe house",
+                        "smirk"
+                    ]
+                }
             }
-    
-            let bernieeeeQuestion = {
-                "questions":[
-                    "Et donc sur",
-                    "La go la c'est peut etre une fille bien. Mais que preferons nous ?",
-                    "Vague, pierre, fantome, loing",
-                    "A l'ultimate, on cherche à l'éviter à tout prix. Mais dans un RPG c'est souvent une bonne nouvelle",
-                    "Cette chanson est vraiment la plus dure de toute... Je n'arrive même pas les 3 premières couleurs...",
-                    "<p id=\"song\" > Oh shit soraka i'm gonna die !! p> 235s"
-                ],
-                "answers":[
-                    "hoth",
-                    "tchoin",
-                    "tsunami",
-                    "drop",
-                    "rouge vert jaune",
-                    "mocking shout"
-                ]
-            }
-    
-            db.put('bernieeeeQuestion',JSON.stringify(bernieeeeQuestion) , function (err) {
-                if (err) res.send({"err4":err.toString()})
-            })
-    
-            db.put('chitaiQuestion',JSON.stringify(chitaiQuestion) , function (err) {
-                if (err) res.send({"err5":err.toString()})
-            })
-    
-            // TODO add 0 in timer for thoses bitches then make the winerFunction
-            db.put('chitaiEnd',99999999999999 , function (err) {
-                if (err) res.send({"err6":err.toString()})
-            })
-    
-            db.put('bernieeeeEnd',99999999999999, function (err) {
-                if (err) res.send({"err7":err.toString()})
-                res.send("ok")
-            })
-        }, 3000);
-    },
-
-    answerQuestion: (player,answer,res)=> {      
-        db.get(player + "Rep", function (err, questionIndex) {
+        }
+        fs.writeFile(filePath,JSON.stringify(data),(err)=>{
             if (err) res.send(err)
             
-            db.get(player + "Question", function(err, value){
-                if (err) res.send(err)
-                
-                result = value.toString()
-                result = JSON.parse(result)
-                
-                console.log(`Answer: ${answer} - RealAnswer:${result.answers[parseInt(questionIndex)]}`);
+            res.send("ok")
+        })
+    },
 
-                if(result.answers.length - 1 == parseInt(questionIndex)){
+    answerQuestion: (player,answer,res)=> {  
+        fs.readFile(filePath, {encoding: 'utf-8'}, function(err,data){
+            if (!err) {
+                data = JSON.parse(data)
+
+                if(data[player].question.answers.length - 1 == data[player].rep) {
                     res.send({
                         "result":true,
                         "over":true
@@ -134,9 +98,10 @@ module.exports = {
                     return
                 }
 
-                if (result.answers[parseInt(questionIndex)] === answer.toLowerCase()){
-                    db.put(player+"Rep", parseInt(questionIndex) + 1, function(err){
-                        if (err) res.send(err)
+                if (data[player].question.answers[data[player].rep] === answer.toLowerCase()){
+                    data[player].rep = data[player].rep + 1
+                    fs.writeFile(filePath,JSON.stringify(data),(err)=>{
+                        if (err) console.log(err)
                         res.send({
                             "result":true,
                             "over":false
@@ -149,57 +114,60 @@ module.exports = {
                     "result":false,
                     "over":false
                 })
-            })
-          })
+
+                
+            } else {
+                console.log(err)
+            }
+        }); 
     },
     
     getCurrentQuestion: (player,res)=> {        
-        db.get(player + "Rep", function (err, currentQuestion) {
-            if (err) res.send(err) // likely the key was not found
+        filePath = path.join(__dirname, 'database.json');
+        fs.readFile(filePath, {encoding: 'utf-8'}, function(err,data){
+            if (!err) {
+                data = JSON.parse(data)
+                console.log(data[player].question.questions[data[player].rep]);
 
-            db.get(player+"Question",function (err,value){
-                if (err) res.send(err)
-                result = value.toString()
-                result = JSON.parse(result)
-                
-                res.send(result.questions[parseInt(currentQuestion)])
-            })
-          })
+                res.send(data[player].question.questions[data[player].rep])
+            } else {
+                res.send({"err":err})
+            }
+        }); 
     },
 
     getSecondLeft: (res)=> {
-        db.get("timer")
-        .then(function(value){
-            let timer = parseInt(value.toString())
-            let date = Date.now()
-            let dateTimer = new Date(timer + (60000 * timeTotal))
-            if (dateTimer - date > 0){
-                res.send({"timer":dateTimer - date})
-            } else{
-                res.send({"timer":0})
+        filePath = path.join(__dirname, 'database.json');
+        fs.readFile(filePath, {encoding: 'utf-8'}, function(err,data){
+            if (!err) {
+                data = JSON.parse(data)
+                let timer = data.timer
+                let date = Date.now()
+                let dateTimer = new Date(timer + (60000 * timeTotal))
+                if (dateTimer - date > 0){
+                    res.send({"timer":dateTimer - date})
+                } else{
+                    res.send({"timer":0})
+                }
+            } else {
+                res.send({"err":err})
             }
-        })
-        .catch((err)=>{
-            res.send(err)
-        })
+        }); 
     },
     getWinner: (res)=> {
-        console.log("hello");
-        
-        db.get("chitaiEnd")
-        .then(function(chitaiEnd){
-            db.get("bernieeeeEnd")
-            .then(function(bernieeeeEnd){
-                console.log(bernieeeeEnd.toString());
-                console.log(parseInt(chitaiEnd));
-                
+        filePath = path.join(__dirname, 'database.json');
+        fs.readFile(filePath, {encoding: 'utf-8'}, function(err,data){
+            if (!err) {
+                data = JSON.parse(data)
                 var result = {
-                    "bernieeee":bernieeeeEnd == null ? 0 : parseInt(bernieeeeEnd),
-                    "chitai":chitaiEnd == null ? 0 : parseInt(chitaiEnd)
+                    "bernieeee":data.bernieeee.end,
+                    "chitai":data.chitai.end,
                 }
                 res.send(JSON.stringify(result))
-            })
-        })
+            } else {
+                res.send({"err":err})
+            }
+        }); 
     },
 }
 
